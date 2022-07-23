ed512aca0ee80c7b8672e55c51fe3275cb9a65ef
Badiyan <e.mailbox.sbv@gmail.com>
2022-07-23 16:10:08 +0300

    Add project files.

---
+++ variableChange/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variableChange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 7;
+            int c = 20;
+
+            /* expected
+            a = 20;
+            b = 1;
+            c = 7;
+            */
+
+            // 1 variant
+
+            (a,b,c) = (c,a,b);
+            Console.WriteLine("a = {0}, \nb = {1}, \nc = {2} ",
+                a, b, c);
+            Console.ReadKey();
+
+            // 2 variant
+
+            (a, b, c) = (c, a, b);
+            Console.WriteLine("a = {0}, \nb = {1}, \nc = {2} ",
+                a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
